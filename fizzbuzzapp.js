0f34dc7c50548528f00159f631336ff5bd2f77da
mariakeeton <maria.keeton@gmail.com>
2015-02-03 07:06:51 -0500

    Added complete js code

--- fizzbuzzapp.js
+++ fizzbuzzapp.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	for (var j = 1; j <= 100; j++) {
+	/*for (var j = 1; j <= 100; j++) {
 		var number=j;
 		if(number % 3 == 0 && number % 5 == 0){
 			number='fizzbuzz'
@@ -11,5 +11,40 @@ $(document).ready(function() {
 		}
 		$('p').append(number+'<br/>');
 		console.log(number);
-	};
+	};*/
+
+	var fizzButton = $('button');
+	var fizzInput = $('input');
+	var errMsg = "";
+		//Add item to list if Add Item button is pressed
+	fizzButton.click(function() {
+		fizzbuzzer(fizzInput.val());
+	})
+	//Add item to list when enter key is pressed
+	fizzInput.keyup(function(e) {
+		if (e.keyCode == 13)
+			fizzbuzzer(fizzInput.val());
+	})
+	function fizzbuzzer(num) {
+		$('ul').empty();
+		if (isNaN(num)) {
+				alert('Enter a Number');
+		}else if(num % 1 !==0){
+			alert('Enter Whole Number');
+		}else if (num % 1 == 0){
+		for (var j = 1; j <= num; j++) {
+			var number=j;
+			if(number % 3 == 0 && number % 5 == 0){
+				number='fizzbuzz';
+			}else if (number % 5 == 0) {
+				number='buzz';
+			}else if (number % 3 == 0) {
+				number='fizz';
+			}
+			$('ul').append('<li>' + number + '</li>');
+			//console.log(number);
+			}
+		};
+		fizzInput.val("");
+	}
 });
